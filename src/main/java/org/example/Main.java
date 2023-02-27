e35e9cc480dd71d4dff1caef8ab58bc94fee71de
Max <michaelcorleone@mailbox.org>
2023-02-27 16:00:39 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
+
 import java.util.Scanner;
 
-class Main{
+class A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
-        int number3 = scanner.nextInt();
-        System.out.println(Math.max(number1, Math.max(number2, number3)));
+        int total = 0, counter = 0, item;
+
+        while ((item = scanner.nextInt()) != 0) {
+            counter++;
+            total += item;
+        }
+        System.out.println((double) total/ counter);
     }
 }
