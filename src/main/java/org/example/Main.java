8992892ad46ea136b7d405aa5e88213f539cc020
Max <michaelcorleone@mailbox.org>
2023-03-01 08:39:07 +0300

    change variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,32 @@
 package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.SortedMap;
 
 class Main{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
-        int number3 = scanner.nextInt();
-        System.out.println(Math.max(number1, Math.max(number2, number3)));
+        Scanner sc = new Scanner(System.in);
+
+        int sizeArray = sc.nextInt();
+
+        int[] array = new int[sizeArray];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+
+        }
+        int a = sc.nextInt(), b = sc.nextInt() ;
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+
+
     }
 }
