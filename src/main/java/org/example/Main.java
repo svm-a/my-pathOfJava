01df750e6d31b48ecab088892254799475840839
Max <michaelcorleone@mailbox.org>
2023-03-05 18:49:29 +0300

    magic squart

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 class HomeWork {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int m = sc.nextInt();
         int n = sc.nextInt();
-
-        int[][] array = new int[m][n];
-        int[][] result = new int[n][m];
+        int[][] array = new int[n][n];
+        int totalX = 0;
+        int totalY = 0;
+        int totalDig1 = 0;
+        int totalDig2 = 0;
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
@@ -19,18 +19,23 @@ class HomeWork {
             }
         }
 
+        boolean flag = true;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                result[j][m - i - 1] = array[i][j];
-                }
-        }
-
-        for (int i = 0; i < result.length; i ++) {
-            for (int j = 0; j < result[i].length; j ++) {
-                System.out.print(result[i][j] + " ");
+                totalX += array[i][j];
+                totalY += array[j][i];
+                totalDig1 += array[j][j];
+                totalDig2 += array[j][array.length - 1 - j];
             }
-            System.out.println();
+            if (totalX == totalY && totalY == totalDig1 && totalDig1 == totalDig2) {
+                totalX = 0;
+                totalY = 0;
+                totalDig1 = 0;
+                totalDig2 = 0;
+            } else {
+                flag = false;
+            }
         }
-
+        System.out.println(flag? "YES" : "NO");
     }
 }
